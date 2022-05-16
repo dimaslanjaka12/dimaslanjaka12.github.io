2f267f16219d324007263f536683c9372b33db98
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-05-17 05:30:06 +0700

    fix redirector

--- source/assets/redirect.js
+++ source/assets/redirect.js
@@ -1,4 +1,5 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function () {
+  //dom is fully loaded, but maybe waiting on images & css files
   const isAdmin = getCookie('cookie_admin');
   const _whitelist = location.host.includes('dimaslanjaka12');
   if (!isAdmin) {
@@ -7,26 +8,20 @@ window.onload = function () {
   } else {
     console.log('you are admin');
   }
-};
+});
 
-function getCookie(cname) {
-  var name = cname + '=';
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
+/**
+ * get cookie by key
+ * @param {string} name
+ * @returns
+ */
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
   for (var i = 0; i < ca.length; i++) {
-    if (window.CP) {
-      if (window.CP.shouldStopExecution(0)) break;
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        if (window.CP.shouldStopExecution(1)) break;
-        c = c.substring(1);
-      }
-      window.CP.exitedLoop(1);
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-  window.CP.exitedLoop(0);
   return null;
 }
