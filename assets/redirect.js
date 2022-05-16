45c1c6420f39de96d8ff50fbf447add07084d449
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-05-17 05:50:55 +0700

    Site updated: 2022-05-17 05:49:23

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,25 +1,27 @@
-window.onload = function () {
-  if (location.host.includes('dimaslanjaka12') && !getCookie('cookie_admin')) {
-    location.replace('${to}');
+document.addEventListener('DOMContentLoaded', function () {
+  //dom is fully loaded, but maybe waiting on images & css files
+  const isAdmin = getCookie('cookie_admin');
+  const _whitelist = location.host.includes('dimaslanjaka12');
+  if (!isAdmin) {
+    if (_whitelist) location.replace('${to}');
+    console.log("you aren't admin");
+  } else {
+    console.log('you are admin');
   }
-};
+});
 
-function getCookie(cname) {
-  var name = cname + '=';
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
+/**
+ * get cookie by key
+ * @param {string} name
+ * @returns
+ */
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
   for (var i = 0; i < ca.length; i++) {
-    if (window.CP.shouldStopExecution(0)) break;
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      if (window.CP.shouldStopExecution(1)) break;
-      c = c.substring(1);
-    }
-    window.CP.exitedLoop(1);
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
-  window.CP.exitedLoop(0);
   return null;
 }
